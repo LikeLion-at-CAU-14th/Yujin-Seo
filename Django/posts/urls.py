0e5db9d03862d43ec5ccb5796289d110463567c2
dalsudang <seoyjsyj0117@gmail.com>
2026-03-27 00:57:11 +0900

    feat: add Category model and Comment model

--- Django/posts/urls.py
+++ Django/posts/urls.py
@@ -1,6 +1,8 @@
+# posts/urls.py
 from django.urls import path
 from posts.views import *
 
+# URL 패턴 정의
 urlpatterns = [
     path('', hello_world, name = 'hello_world'),
     path('page', index, name='my-page'),
