cee164cc85d1e98111a5355adcd1078a3eb2b13f
dalsudang <seoyjsyj0117@gmail.com>
2026-04-03 14:15:33 +0900

    feat: W5 CRED Session

--- Django/posts/urls.py
+++ Django/posts/urls.py
@@ -4,8 +4,10 @@ from posts.views import *
 
 # URL 패턴 정의
 urlpatterns = [
-    path('', hello_world, name = 'hello_world'),
-    path('page', index, name='my-page'),
-        path('<int:id>', get_post_detail)
+    #path('', hello_world, name = 'hello_world'),
+    #path('page', index, name='my-page'),
+    #path('<int:id>', get_post_detail),
 
+    path('', post_list, name = "post_list"), # Post 생성, 전체 조회
+    path('<int:post_id>/', post_detail, name = "post_detail") # Post 단일조회, 수정, 삭제
     ]
