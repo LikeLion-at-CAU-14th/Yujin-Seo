2e26c4e62e87c8bcd0c4d00e5a1c43cf9bbc784b
dalsudang <seoyjsyj0117@gmail.com>
2026-05-31 16:14:30 +0900

    fix: 프론트 최종 배포 도메인 CORS 허용

--- Django/config/settings.py
+++ Django/config/settings.py
@@ -164,6 +164,8 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
     "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "http://guestbook-box.kro.kr",
 ]
 
 LOGGING = {
